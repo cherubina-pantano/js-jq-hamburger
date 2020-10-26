a33569b8df8568e18c023b0b73d3b2abcd985da8
cherubina-pantano <cherubina.pantano@gmail.com>
2020-10-26 16:09:51 +0100

    Humburger-menu hide

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var navBars = $('.header-right > a');
+var close = $('.close');
 
 navBars.click(function () {
   $('.hamburger-menu').show();
 
+})
+
+close.click( function () {
+  $('.hamburger-menu').hide();
 })
